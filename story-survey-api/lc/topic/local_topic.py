34f1468f69ed0b7a809fd8f84f50eac5bc554b43
Ishrat Rahman Sami <ishrat@thebyte9.com>
2020-09-06 18:58:42 +0100

    @wip lc

--- story-survey-api/lc/topic/local_topic.py
+++ story-survey-api/lc/topic/local_topic.py
@@ -6,44 +6,59 @@ class LocalTopic(CWR):
         super().__init__()
         return
     
-    def get(self, text, allowedScoreRatio = 0.5):
+    def get(self, text, allowedScoreRatio = 0.1):
         self._reset()
         self.loadSentences(text)
         self.loadScore()
         self.setMaxAllowedRadius(allowedScoreRatio)
+        self.sort()
+        self.properNouns = list(set(self.getProperNouns()))
         
         for word in self.wordsInfo.keys():
             self._addPriorityWords(self.wordsInfo[word])
         
         topics = self.filteredWordsByType
-        topics['joint_proper_noun'] = self.getProperNouns()
-         
-        return topics
+        return self.getTopicsToDisplay()
     
+    def getTopicsToDisplay(self, limit = 5):
+        wordsToDisplay = []
+        for type in ['noun', 'adjective', 'adverb', 'negative', 'adverb', 'verb', 'positive']:
+            wordsByType = self.filteredWordsByType[type][0:limit]
+            for word in wordsByType:
+                word['color_group'] = type
+                word['name'] = self._getDisplayName(word)
+                wordsToDisplay.append(word)
+                
+        return wordsToDisplay
     
     def _addPriorityWords(self, word):
         if word['score'] < self.maxAllowedScore:
             return
 
-        displayName = self._getDisplayName(word)
         for typeName in self.wordPosGroups.keys():
             if word['type'] in self.wordPosGroups[typeName]:
-                self.filteredWordsByType[typeName].append(displayName)
+                self.filteredWordsByType[typeName].append(word)
 
         if word['stemmed_word'] in self.positiveWords:
-            self.filteredWordsByType['positive'].append(displayName)
+            self.filteredWordsByType['positive'].append(word)
+            self.filteredWordsByType[typeName].pop()
 
         if word['stemmed_word'] in self.negativeWords:
-            self.filteredWordsByType['negative'].append(displayName)
-        
+            self.filteredWordsByType['negative'].append(word)
+            self.filteredWordsByType[typeName].pop()
         return
     
     def _getDisplayName(self, word):
-        return word['pure_word'] + ' - ' + str(word['score'])
+        if self.properNouns and word['color_group']  == 'noun':
+            for properName in self.properNouns:
+                if word['pure_word']in properName:
+                    return properName;
+                
+        return word['pure_word'][0].upper() + word['pure_word'][1:]
     
     def _reset(self):
         super()._reset()
         self.filteredWordsByType = {}
-        for type in ['joint_proper_noun', 'proper_noun', 'noun', 'adjective', 'adverb', 'verb', 'positive', 'negative']:
+        for type in ['proper_noun', 'noun', 'adjective', 'adverb', 'verb', 'positive', 'negative']:
             self.filteredWordsByType[type] = []
         return
--- story-survey-api/lc/topic/word.py
+++ story-survey-api/lc/topic/word.py
@@ -2,8 +2,10 @@ import re
 from nltk import word_tokenize, pos_tag
 from nltk.stem.porter import PorterStemmer
 import utility
+import operator
+
 class Word:
-    
+
     def __init__(self):
         super().__init__()
         self.stopWords = utility.Utility.getStopWords()
@@ -13,27 +15,26 @@ class Word:
         self.__loadGroups()
         return
 
-    
     def loadSentences(self, text):
         self._reset()
         text = self.__clean(text)
         words = self.__getWords(text)
         if not len(words):
             return
-        
+
         for word in words:
             (word, type) = word
             word = self.__cleanWord(word)
-            
+
             if type in self.punctuationTypes:
                 if len(self.currentSentence) > 1:
                     # If more than one word than add as sentence
                     self.sentences.append(self.currentSentence)
                 self.currentSentence = []
-                
+
             if (len(word) < self.minWordSize) or (word in self.stopWords):
                 continue
-			
+
             wordKey = self._addWordInfo(word, type)
             if wordKey and (wordKey not in self.currentSentence):
                 self.currentSentence.append(wordKey)
@@ -44,8 +45,21 @@ class Word:
                 self.sentences.append(self.currentSentence)
 
         return
-    
-        
+
+    def sort(self, attribute='score'):
+        if not len(self.wordsInfo):
+            return
+
+        sortedWords = {}
+        contributors = self.wordsInfo.values()
+
+        for value in sorted(contributors, key=operator.itemgetter(attribute), reverse=True):
+            sortedWords[value['stemmed_word']] = value
+
+        self.wordsInfo = sortedWords
+        return
+
+
     def _addWordInfo(self, word, type):
         if not word or (type not in self.allowedPOSTypes):
             return None
@@ -98,11 +112,46 @@ class Word:
     def __cleanWord(self, word):
     	return re.sub('[^a-zA-Z0-9]+', '', word)
 
-
+    '''
+    CC coordinating conjunction
+    CD cardinal digit
+    DT determiner
+    EX existential there (like: “there is” … think of it like “there exists”)
+    FW foreign word
+    IN preposition/subordinating conjunction
+    JJ adjective ‘big’
+    JJR adjective, comparative ‘bigger’
+    JJS adjective, superlative ‘biggest’
+    LS list marker 1)
+    MD modal could, will
+    NN noun, singular ‘desk’
+    NNS noun plural ‘desks’
+    NNP proper noun, singular ‘Harrison’
+    NNPS proper noun, plural ‘Americans’
+    PDT predeterminer ‘all the kids’
+    POS possessive ending parent’s
+    PRP personal pronoun I, he, she
+    PRP$ possessive pronoun my, his, hers
+    RB adverb very, silently,
+    RBR adverb, comparative better
+    RBS adverb, superlative best
+    RP particle give up
+    TO, to go ‘to’ the store.
+    UH interjection, errrrrrrrm
+    VB verb, base form take
+    VBD verb, past tense took
+    VBG verb, gerund/present participle taking
+    VBN verb, past participle taken
+    VBP verb, sing. present, non-3d take
+    VBZ verb, 3rd person sing. present takes
+    WDT wh-determiner which
+    WP wh-pronoun who, what
+    WP$ possessive wh-pronoun whose
+    WRB wh-abverb where, when
+    '''
     def __loadGroups(self):
         self.wordPosGroups = {}
-        self.wordPosGroups['proper_noun'] = ['NNP', 'NNPS']
-        self.wordPosGroups['noun'] = ['NN', 'NNS']
+        self.wordPosGroups['noun'] = ['NN', 'NNS', 'NNP', 'NNPS']
         self.wordPosGroups['adjective'] = ['JJ', 'JJR', 'JJS']
         self.wordPosGroups['adverb'] = ['RB', 'RBR', 'RBS']
         self.wordPosGroups['verb'] = ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
